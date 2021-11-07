1edbf749d0c8fcfce476f322d86de6e11b5bb5d0
cjlaaa <cjlaaa@gmail.com>
2021-11-07 22:50:54 +0800

    导入游戏资源,创建卡牌预制体

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,36 @@
+﻿
+public class Card
+{
+    public int Id;
+    public string CardName;
+
+    public Card(int id, string cardName)
+    {
+        this.Id = id;
+        this.CardName = cardName;
+    }
+}
+
+public class MonsterCard : Card
+{
+    public int Attack;
+    public int HealthPoint;
+    public int HealthPointMax;
+
+    public MonsterCard(int id, string cardName, int attack, int heathPoint, int healthPointMax):base (id,cardName)
+    {
+        this.Attack = attack;
+        this.HealthPoint = heathPoint;
+        this.HealthPointMax = healthPointMax;
+    }
+}
+
+public class SpellCard : Card
+{
+    public string Effect;
+    
+    public SpellCard(int id, string cardName, string effect):base (id,cardName)
+    {
+        this.Effect = effect;
+    }
+}
---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Text nameText;
+    public Text attackText;
+    public Text healthText;
+    public Text effectText;
+
+    public Image backgroundImage;
+
+    public Card card;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowCard()
+    {
+        nameText.text = card.CardName;
+        if (card is MonsterCard)
+        {
+            var monster = card as MonsterCard;
+            attackText.text = monster.Attack.ToString();
+            healthText.text = monster.HealthPoint.ToString();
+            
+            effectText.gameObject.SetActive(false);
+        }
+        else if (card is SpellCard)
+        {
+            var spell = card as SpellCard;
+            effectText.text = spell.Effect;
+            
+            attackText.gameObject.SetActive(false);
+            healthText.gameObject.SetActive(false);
+        }
+    }
+}
